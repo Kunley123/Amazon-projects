882c3ee73630d4013bdd0489c31b26540b70b26b
Adebanjo Adekunle <Adebanjokunle56@gmail.com>
2024-07-01 12:48:26 +0100

    Import product from product from product.js

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -1,6 +1,6 @@
 // This is a data structure because it structures or organises a data as shown in the output
 
-const products = [
+export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
