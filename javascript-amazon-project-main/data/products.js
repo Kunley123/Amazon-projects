fb106d6c11d4d537741e23eaa4f8799c18f2c840
Adebanjo Adekunle <Adebanjokunle56@gmail.com>
2024-07-16 14:01:46 +0100

    Save data and generate HTML for payment summary

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -1,5 +1,17 @@
 // This is a data structure because it structures or organises a data as shown in the output
 
+export function getProduct(productId) {
+  let matchingProduct;
+
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+  return matchingProduct;
+
+}
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
